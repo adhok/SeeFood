134563aaf9c694949299c909381ce3dea57c7ca7
Pradeep Adhokshaja <padhokshaja@gmail.com>
2022-09-04 22:13:48 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -14,11 +14,11 @@ from tensorflow.keras.utils import load_img, img_to_array
 
 
 def infer(img):
-    
-    
+
+
     indian_food_final = pd.read_csv('indian_food_final.csv')
-    
-    
+
+
     img_width, img_height = 224, 224
     img = load_img(img, target_size = (img_width, img_height))
     img = img_to_array(img)
@@ -30,9 +30,9 @@ def infer(img):
     # Loads the weights
     model.load_weights("training_1/cp.ckpt")
 
-    predicted_vector = model.predict(img)
-    
-    print(predicted_vector.shape)
+    predicted_vector = model.predict(img,use_multiprocessing=True)
+
+    #print(predicted_vector.shape)
 
     ## Convert the vector to list
 
@@ -51,7 +51,7 @@ def infer(img):
 
 
     top_three = sort_index(predicted_vector_list)[:3]
-    
+
     print(top_three)
     with open('class_mappings.json') as json_file:
         data = json.load(json_file)
@@ -61,50 +61,50 @@ def infer(img):
 
 
     #### the top three predictions
-    
+
     list_of_predictions = []
-    
+
     list_of_prep_times  = []
-    
+
     list_of_regions = []
-    
+
     list_of_ingredients= []
 
     for index in top_three:
 
         list_of_predictions.append(data[index])
-        
+
         prep_time = indian_food_final[indian_food_final['name']==data[index]]['prep_time'].iloc[0]
-        
-        
+
+
         list_of_prep_times.append(prep_time)
-        
+
         region = indian_food_final[indian_food_final['name']==data[index]]['region'].iloc[0]
-        
+
         list_of_regions.append(region)
-        
+
         ingredients = indian_food_final[indian_food_final['name']==data[index]]['ingredients'].iloc[0]
-        
+
         list_of_ingredients.append(ingredients)
-        
-        
-        
+
+
+
     ##### return predictions, prep times, list of regions and ingredients
-        
-    
-    
+
+
+
     top_three_values_prediction = sorted(predicted_vector_list,reverse=True)[:3]
-    
-    
-    
+
+
+
     return(list_of_predictions,list_of_prep_times,list_of_regions,list_of_ingredients,top_three_values_prediction)
 
 
-    
-    
-    
-    
-    
+
+
+
+
+
 
 
 
